97778b3803e2d6a45eafa852b91e99ef44fd41d5
persjess <jesse.persinger.hvac@gmail.com>
2023-09-28 12:41:19 -0400

    prep for deploy

--- anvil-procurement/anvil-backend/db.js
+++ anvil-procurement/anvil-backend/db.js
@@ -1,14 +1,14 @@
 const Sequelize = require('sequelize');
 
-// Replace these values with your actual database credentials
-// const sequelize = new Sequelize('anvil-procurement', 'postgres', '12345', {
-//   host: 'localhost',
-//   dialect: 'postgres'
-// });
-
-const sequelize = new Sequelize('anvilDb', 'Mau5trap91', 'password', {
-  host: 'rds-anvil-db.cnilt0q0gjft.us-east-1.rds.amazonaws.com',
+//Replace these values with your actual database credentials
+const sequelize = new Sequelize(process.env.DATABASE_URL || 'anvil-procurement', 'postgres', '12345', {
+  host: 'localhost',
   dialect: 'postgres'
 });
+
+// const sequelize = new Sequelize('anvilDb', 'Mau5trap91', 'password', {
+//   host: 'rds-anvil-db.cnilt0q0gjft.us-east-1.rds.amazonaws.com',
+//   dialect: 'postgres'
+// });
 
 module.exports = sequelize;
